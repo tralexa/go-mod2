b4e35493309553305a28e5a918f68232fba340ba
Abugov Alex <alexa@traiana.com>
2019-11-28 08:11:24 +0200

    rollback 1

--- cmd/cmd2b/cmd2b.go
+++ cmd/cmd2b/cmd2b.go
@@ -2,9 +2,10 @@ package cmd2b
 
 import (
 	"github.com/tralexa/go-mod1/pkg/pkg1b"
-	"github.com/tralexa/go-mod1/pkg/pkg1f")
+	//"github.com/tralexa/go-mod1/pkg/pkg1f"
+	)
 
 func main() {
 	pkg1b.Do()
-	pkg1f.Do()
+	//pkg1f.Do()
 }
